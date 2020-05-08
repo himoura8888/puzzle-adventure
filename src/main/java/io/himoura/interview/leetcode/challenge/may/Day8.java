f9de79b76b0ba5d677f5660acb26d9557ab48d81
himoura8888 <style2kes@msn.com>
2020-05-08 18:13:04 +0200

    (feature) [LeetCode] Challenge May Day 8

---
+++ src/main/java/io/himoura/interview/leetcode/challenge/may/Day8.java
@@ -0,0 +1,28 @@
+package io.himoura.interview.leetcode.challenge.may;
+
+/**
+ * <b>Check If It Is a Straight Line</b>
+ * <p>
+ * You are given an array coordinates, coordinates[i] = [x, y], where [x, y]
+ * represents the coordinate of a point. Check if these points make a straight
+ * line in the XY plane.
+ * <dl>
+ * <dt>Example 1:</dt>
+ * <dd></br>
+ * Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]] </br>
+ * Output: true</dd>
+ * <dt>Example 2:</dt>
+ * <dd></br>
+ * Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]] </br>
+ * Output: false</dd>
+ * </dl>
+ * </p>
+ * 
+ * @version 8 mai 2020
+ */
+public class Day8 {
+	public boolean checkStraightLine(int[][] coordinates) {
+		boolean result = false;
+		return result;
+	}
+}
---
+++ src/test/java/io/himoura/interview/leetcode/challenge/may/Day8Test.java
@@ -0,0 +1,22 @@
+package io.himoura.interview.leetcode.challenge.may;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Day8Test {
+
+	@DisplayName("LeetCode Example")
+	@Test
+	void shouldReturnLeetCodeExpectedResult() {
+
+		// Given
+
+		// When
+
+		// Then
+		fail("Not implemented yet");
+	}
+
+}
