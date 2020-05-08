61b332d567a9557e8224ee0b6ab1e24c4b8d159e
himoura8888 <style2kes@msn.com>
2020-05-08 18:21:01 +0200

    Add unit test with LeetCode example

--- src/test/java/io/himoura/interview/leetcode/challenge/may/Day8Test.java
+++ src/test/java/io/himoura/interview/leetcode/challenge/may/Day8Test.java
@@ -1,6 +1,7 @@
 package io.himoura.interview.leetcode.challenge.may;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,11 +13,16 @@ class Day8Test {
 	void shouldReturnLeetCodeExpectedResult() {
 
 		// Given
-
+		final Day8 day8 = new Day8();
 		// When
+		boolean resultExample1 = day8
+				.checkStraightLine(new int[][] { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 5 }, { 5, 6 }, { 6, 7 } });
+		boolean resultExample2 = day8
+				.checkStraightLine(new int[][] { { 1, 1 }, { 2, 2 }, { 3, 4 }, { 4, 5 }, { 5, 6 }, { 7, 7 } });
 
 		// Then
-		fail("Not implemented yet");
+		assertTrue(resultExample1);
+		assertFalse(resultExample2);
 	}
 
 }
